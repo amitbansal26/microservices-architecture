28e9dcb65d980a387c7815640007f7cd4b8c2ced
Bansal <amit.bansal@capgemini.com>
2017-07-11 00:14:15 +0530

    Added config server

---
+++ config-server/src/main/java/com/ref/arch/config/ConfigServiceApplication.java
@@ -0,0 +1,17 @@
+package com.ref.arch.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
