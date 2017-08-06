527f2bebd81a81479bb05e944808d73aede9a4b3
Bansal <amit.bansal@capgemini.com>
2017-08-06 20:40:04 +0530

    updated

--- sample-api-gateway/src/main/java/com/sample/api/gateway/SampleApiGatewayApplication.java
+++ sample-api-gateway/src/main/java/com/sample/api/gateway/SampleApiGatewayApplication.java
@@ -2,10 +2,13 @@ package com.sample.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient	
 public class SampleApiGatewayApplication {
 
 	public static void main(String[] args) {
